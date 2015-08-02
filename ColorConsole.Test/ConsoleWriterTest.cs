052b67bb13994395fd6126c4a3dee9f2d4446fcb
Richard Kirby <rk755@york.ac.uk>
2015-08-02 14:54:56 +0100

    Adding NUnit to COnsoleWriterTest fixture.

--- ColorConsole.Test/ConsoleWriterTest.cs
+++ ColorConsole.Test/ConsoleWriterTest.cs
@@ -1,7 +1,14 @@
+using NUnit.Framework;
+
 namespace ColorConsole.Test
 {
+    [TestFixture]
     internal sealed class ConsoleWriterTest
     {
-        
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
     }
 }
