e46dca56d724006443e255d13b6206e05c4266b9
Richard Kirby <rk755@york.ac.uk>
2015-08-02 15:20:49 +0100

    Added test to verify invocation of console write.

--- ColorConsole.Test/ConsoleWriterTest.cs
+++ ColorConsole.Test/ConsoleWriterTest.cs
@@ -1,3 +1,4 @@
+using Moq;
 using NUnit.Framework;
 
 namespace ColorConsole.Test
@@ -5,10 +6,22 @@ namespace ColorConsole.Test
     [TestFixture]
     internal sealed class ConsoleWriterTest
     {
+        private Mock<IConsoleWrapper> console;
+        private ConsoleWriter writer;
+
         [SetUp]
         public void SetUp()
         {
+            console = new Mock<IConsoleWrapper>();
+            writer = new ConsoleWriter(console.Object);
+        }
 
+        [Test]
+        public void ConsoleWrite_IsInvoked_OnWriteWithNoColors()
+        {
+            string message = "oh hai!";
+            writer.Write(message);
+            console.Verify(c => c.Write(message), Times.Once);
         }
     }
 }
