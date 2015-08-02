d5d1a68c469e6e9c978a504a5acede31bfe074c9
Richard Kirby <rk755@york.ac.uk>
2015-08-02 15:11:47 +0100

    ConsoleWriter now implements interface. To be implemented.

--- ColorConsole/ConsoleWriter.cs
+++ ColorConsole/ConsoleWriter.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace ColorConsole
 {
-    public sealed class ConsoleWriter
+    public sealed class ConsoleWriter : IConsoleWriter
     {
         private readonly IConsoleWrapper console;
 
@@ -14,6 +14,46 @@ namespace ColorConsole
         internal ConsoleWriter(IConsoleWrapper console)
         {
             this.console = console;
+        }
+
+        public void Write(string message)
+        {
+
+        }
+
+        public void Write(string message, ConsoleColor foreGroundColor)
+        {
+
+        }
+
+        public void Write(string message, ConsoleColor foreGroundColor, ConsoleColor backGroundColor)
+        {
+
+        }
+
+        public void WriteLine(string message)
+        {
+
+        }
+
+        public void WriteLine(string message, ConsoleColor foreGroundColor)
+        {
+
+        }
+
+        public void WriteLine(string message, ConsoleColor foreGroundColor, ConsoleColor backGroundColor)
+        {
+
+        }
+
+        public void SetForeGroundColor(ConsoleColor color)
+        {
+
+        }
+
+        public void SetBackGroundColor(ConsoleColor color)
+        {
+
         }
     }
 }
