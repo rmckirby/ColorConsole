8490b7780d0cfae545d3c03b9a63d8ccc90061fb
Richard Kirby <rk755@york.ac.uk>
2015-08-04 10:39:31 +0100

    Added generic Write overloads.

--- ColorConsole/IConsoleWriter.cs
+++ ColorConsole/IConsoleWriter.cs
@@ -4,10 +4,18 @@ namespace ColorConsole
 {
     public interface IConsoleWriter
     {
+        void Write<T>(T message);
+        void Write<T>(T message, ConsoleColor foreground);
+        void Write<T>(T message, ConsoleColor foreground, ConsoleColor background);
+
+        void WriteLine<T>(T message);
+        void WriteLine<T>(T message, ConsoleColor foreground);
+        void WriteLine<T>(T message, ConsoleColor foreground, ConsoleColor background);
+
         void Write(string message);
         void Write(string message, ConsoleColor foreground);
         void Write(string message, ConsoleColor foreground, ConsoleColor background);
-        
+
         void WriteLine(string message);
         void WriteLine(string message, ConsoleColor foreground);
         void WriteLine(string message, ConsoleColor foreground, ConsoleColor background);
