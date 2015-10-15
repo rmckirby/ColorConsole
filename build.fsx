28ea28d1eab58a30e649942a996729d7d1367760
Richard Kirby <rk755@york.ac.uk>
2015-10-15 16:43:47 +0100

    Added code coverage tools. These are only run in a non Mono environment.

--- build.fsx
+++ build.fsx
@@ -1,12 +1,22 @@
 #r @"packages/FAKE/tools/FakeLib.dll"
 
 open Fake
+open Fake.OpenCoverHelper
+open Fake.ReportGeneratorHelper
 
-let buildDir = "./build/"
-let testDir  = "./test/"
+let buildDir     = "./build/"
+let testDir      = "./test/"
+let reportDir    = "./reports/"
+let coverageDir  = testDir + "coverage/"
+let coverageFile = coverageDir + "code-coverage.xml"
 
 Target "Clean" (fun _ ->
     CleanDirs [buildDir; testDir;]
+)
+
+Target "CreateDirs" (fun _ ->
+    CreateDir coverageDir
+    CreateDir reportDir
 )
 
 Target "BuildCore" (fun _ ->
@@ -31,14 +41,38 @@ Target "UnitTest" (fun _ ->
         )
 )
 
-Target "Default" (fun _ ->
-    trace "ColorConsole"
+Target "UnitTest_WithCoverage" (fun _ ->
+    "/noshadow " + testDir + "ColorConsole.Test.dll "
+        |> OpenCover (fun p ->
+            {p with
+                ExePath = "packages/tools/OpenCover/tools/OpenCover.Console.exe"
+                TestRunnerExePath = "packages/NUnit.Runners/tools/nunit-console.exe"
+                Output = coverageFile
+                Filter = "+[ColorConsole*]* -[*.Test*]*"
+                Register = RegisterUser
+            }
+        )
 )
 
+Target "GenerateCoverageReport" (fun _ ->
+    [coverageFile;]
+        |> ReportGenerator (fun p ->
+            {p with
+                ExePath = "packages/tools/ReportGenerator/tools/ReportGenerator.exe"
+                TargetDir = reportDir
+            }
+        )
+)
+
+Target "Default" DoNothing
+
 "Clean"
+    ==> "CreateDirs"
     ==> "BuildCore"
     ==> "BuildTest"
-    ==> "UnitTest"
+    =?> ("UnitTest", isMono)
+    =?> ("UnitTest_WithCoverage", not isMono)
+    =?> ("GenerateCoverageReport", not isMono)
     ==> "Default"
-    
+
 RunTargetOrDefault "Default"
